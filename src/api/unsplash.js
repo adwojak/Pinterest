aa4c8ab978d84ec24a089195faeabab1ce4d3e3a
Artur Dwojak <dwojaka207@gmail.com>
2020-02-22 12:31:22 +0100

    Removed unused files

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'https://api.unsplash.com',
-    headers: {
-        Authorization: 'Client-ID 30bcaccb65c2faf7a18cb3abfb9f7567eb761270a34c06f58e02ab3f00d9e9f3'
-    }
-});
